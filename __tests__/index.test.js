a6d0635c46c05c12ecff4e3f27b5869a0f721397
Anton Arzamastsev <tonich@sbor.net>
2024-12-06 21:39:16 +0300

    feat: add test for a long string through fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import fs from 'fs';
 import reverse from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh');
   expect(reverse('')).toEqual('');
+
+  const text = readFile('someLongText.txt').trim();
+  const result = readFile('reversedLongText.txt').trim();
+  expect(reverse(text)).toEqual(result);
 });
